82069ac7c6131ae18ef664310827439d2c8a483f
ufolog <wisenss@gmail.com>
2014-12-01 20:31:15 +0200

    1

--- HW_methods/HW_methods_1/Program.cs
+++ HW_methods/HW_methods_1/Program.cs
@@ -10,7 +10,52 @@ namespace HW_methods_1
     {
         static void Main(string[] args)
         {
+            System.Console.Write("Enter the first operand: ");
+            double operand1 = double.Parse(System.Console.ReadLine());
 
+            System.Console.Write("Enter the second operand: ");
+            double operand2 = double.Parse(System.Console.ReadLine());
+
+            System.Console.Write("Enter the sign of arithmetic operation: ");
+            var sign = System.Console.ReadLine();
+            switch (sign)
+            {
+                case "+":
+                    Console.WriteLine(add(operand1, operand2));
+                    break;
+                case "-":
+                    Console.WriteLine(sub(operand1, operand2));
+                    break;
+                case "*":
+                    Console.WriteLine(Mul(operand1, operand2));
+                    break;
+                case "/":
+                       div(operand1, operand2);
+                    break;
+            }
         }
+        static double add(double a, double b)
+        {
+            return a + b;
+        }
+        static double sub(double a, double b)
+        {
+            return a - b;
+        }
+        static double Mul(double a, double b)
+        {
+            return a * b;
+        }
+        static double div(double a, double b)
+        {
+            if (b==0)
+            {
+               System.Console.Write("Attempt to div by zero detected. This is not allowed");
+                return -1;
+            }
+            else return a / b;
+        }
+
+
     }
 }
